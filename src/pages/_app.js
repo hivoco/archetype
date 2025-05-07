6d66bb48592a1e0e632264c6bae3d2b318accd4d
ranjanhivoco <ranjan@hivoco.com>
2025-05-07 14:40:42 +0530

    add meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,17 +7,54 @@ import {
 } from "@/context/ArchetypeContext";
 import { useContext } from "react";
 import { QuizDataProvider } from "@/context/QuizDataContext";
+import Head from "next/head";
+// import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }) {
   return (
-    <QuizDataProvider>
-      <DataProvider>
-        <ResultProvider>
-          <ArchetypeProvider>
-            <Component {...pageProps} />
-          </ArchetypeProvider>
-        </ResultProvider>
-      </DataProvider>
-    </QuizDataProvider>
+    <>
+      <Head>
+        <meta property="og:type" content="website" />
+
+        <meta property="og:url" content="https://ofexassessments.com/" />
+
+        <meta
+          property="og:title"
+          content="Discover Your Career Archetype – Take the Free Quiz"
+        />
+
+        <meta
+          property="og:description"
+          content="Uncover your unique career personality with our quick quiz. Understand what motivates you at work and how to make better career choices."
+        />
+
+        <meta
+          name="description"
+          content="Legends Face Off is coming to India! Play this game, answer fun questions about FC Barcelona and Real Madrid, and win a chance to meet the players!"
+        />
+
+        <meta
+          name="keywords"
+          content="career quiz, career archetype, personality quiz, career personality test, work motivation, career choices, free career test, discover career path, career assessment, job personality quiz"
+        />
+
+        <meta property="og:image" content="/images/logo.png" />
+        <link rel="icon" href="" />
+
+        <title>of Experience </title>
+      </Head>
+
+      <QuizDataProvider>
+        <DataProvider>
+          <ResultProvider>
+            <ArchetypeProvider>
+              <Component {...pageProps} />
+            </ArchetypeProvider>
+          </ResultProvider>
+        </DataProvider>
+      </QuizDataProvider>
+
+      {/* <GoogleAnalytics gaId="" /> */}
+    </>
   );
 }
